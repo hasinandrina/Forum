c5c1515cc2dc9333e98ff5f74ab40761daf88c73
hasinandrina <hrakotondrabe@ingenosya.mg>
2013-03-19 10:35:41 +0300

    Implémentation de l'appli forum

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewRails::Application.config.secret_token = '81d02d5d7b33c6c00f3316af3087660122661e7be3de7474bbb01aa8fcb83261b156f90f1c10e664b3a4fae325540522236de235f78d029c08f1deb8f69d2798'
